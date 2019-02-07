2bcd679a9c1243fe65299e588dfd6fe706c375d7
Jared Michael A Juan <jaredmichael.a_juan@yahoo.com>
2019-02-07 15:48:08 -0800

    updated yml.file

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 'use strict';
-exports.DATABASE_URL =
-    process.env.DATABASE_URL ||
-    global.DATABASE_URL ||
+
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+    "mongodb://my-new-user:mnu_pw-001@ds255794.mlab.com:55794/goals"
+
+exports.TEST_DATABASE_URL || 
+    "mongodb://localhost/goals-app;"
+
+global.DATABASE_URL ||
     'mongodb://localhost/goals-app';
+
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
