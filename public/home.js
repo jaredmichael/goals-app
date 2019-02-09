f76ceaf1804b7d4ad85cbb3f4bbacef5072ea13b
Jared Michael A Juan <jaredmichael.a_juan@yahoo.com>
2019-02-08 16:42:20 -0800

    removed mock test data and unecessary code

--- public/home.js
+++ public/home.js
@@ -1,19 +1,5 @@
 $(function() {
     getGoals();
-    // for (let i = 0; i < MOCK_STATUS_UPDATES.data.length; i++) {
-    //     $("#goals-list").append(
-    //         `<li>
-    //             <h2>${MOCK_STATUS_UPDATES.data[i].goal}</h2>
-    //             <h3>${MOCK_STATUS_UPDATES.data[i].mantra}</h3>
-    //             <form id="status">
-    //                 <input class="in-progress" type="radio" name="goal-status" value="in-progress" ${MOCK_STATUS_UPDATES.data[i].status === 'inprogress' ? 'checked' : ''} data-id="${MOCK_STATUS_UPDATES.data[i].goalId}"> In-Progress</br>
-    //                 <input class="achieved" type="radio" name="goal-status" value="achieved" ${MOCK_STATUS_UPDATES.data[i].status === 'achieved' ? 'checked' : ''} data-id="${MOCK_STATUS_UPDATES.data[i].goalId}"> Achieved</br>
-    //                 <button class="js-edit" type="button" data-id="${MOCK_STATUS_UPDATES.data[i].goalId}">Edit Goal</button>
-    //                 <button class="js-delete" type="button" data-id="${MOCK_STATUS_UPDATES.data[i].goalId}">Delete Goal</button>
-    //             </form>
-    //         </li>`
-    //     )
-    // }
     watchForm();
 })
 
--- users/router.js
+++ users/router.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 const jsonParser = bodyParser.json();
 
-// Post to register a new user
 router.post('/', jsonParser, (req, res) => {
   const requiredFields = ['username', 'password'];
   const missingField = requiredFields.find(field => !(field in req.body));
@@ -36,13 +35,6 @@ router.post('/', jsonParser, (req, res) => {
     });
   }
 
-  // If the username and password aren't trimmed we give an error.  Users might
-  // expect that these will work without trimming (i.e. they want the password
-  // "foobar ", including the space at the end).  We need to reject such values
-  // explicitly so the users know what's happening, rather than silently
-  // trimming them and expecting the user to understand.
-  // We'll silently trim the other fields, because they aren't credentials used
-  // to log in, so it's less of a problem.
   const explicityTrimmedFields = ['username', 'password'];
   const nonTrimmedField = explicityTrimmedFields.find(
     field => req.body[field].trim() !== req.body[field]
@@ -63,8 +55,6 @@ router.post('/', jsonParser, (req, res) => {
     },
     password: {
       min: 8,
-      // bcrypt truncates after 72 characters, so let's not give the illusion
-      // of security by storing extra (unused) info
       max: 72
     }
   };
@@ -93,14 +83,12 @@ router.post('/', jsonParser, (req, res) => {
   }
 
   let {username, password} = req.body;
-  // Username and password come in pre-trimmed, otherwise we throw an error
-  // before this
+
 
   return User.find({username})
     .count()
     .then(count => {
       if (count > 0) {
-        // There is an existing user with the same username
         return Promise.reject({
           code: 422,
           reason: 'ValidationError',
@@ -108,7 +96,6 @@ router.post('/', jsonParser, (req, res) => {
           location: 'username'
         });
       }
-      // If there is no existing user, hash the password
       return User.hashPassword(password);
     })
     .then(hash => {
@@ -121,8 +108,6 @@ router.post('/', jsonParser, (req, res) => {
       return res.status(201).json(user.serialize());
     })
     .catch(err => {
-      // Forward validation errors on to the client, otherwise give a 500
-      // error because something unexpected has happened
       if (err.reason === 'ValidationError') {
         return res.status(err.code).json(err);
       }
